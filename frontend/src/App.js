47bbbb577f0d34bd9ba676b647cd1e1a56127e11
avikadhao <avikadhao137@gmail.com>
2025-03-21 14:26:01 +0530

    Create_React_App

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">MERNify</a>
+      </header>
+      <main>
+        List Product
+      </main>
+    </div>
+  );
+}
+
+export default App;
